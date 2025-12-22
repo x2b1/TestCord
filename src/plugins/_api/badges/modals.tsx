7ae989a3a8eef453436a672d5a4f5a5057737ec5
x2b <mokihbbjjj@gmail.com>
2025-12-22 16:18:22 +0100

    change the donate button to my donate link

--- src/plugins/_api/badges/modals.tsx
+++ src/plugins/_api/badges/modals.tsx
@@ -69,8 +69,7 @@ export function TestCordDonorModal() {
     const modalKey = openModal(props => (
         <ErrorBoundary noop onError={() => {
             closeModal(modalKey);
-            // Will get my own in the future
-            VencordNative.native.openExternal("https://github.com/sponsors/thororen1234");
+            VencordNative.native.openExternal("https://ko-fi.com/x2bkaneki");
         }}>
             <ModalRoot {...props}>
                 <ModalHeader>
