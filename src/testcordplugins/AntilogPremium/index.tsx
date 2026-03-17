7b17019ebce590bb6105812097d889ab6b9642ec
x2b <mokihbbjjj@gmail.com>
2026-03-17 21:07:33 +0100

    improve antilogpremium's description

--- src/testcordplugins/AntilogPremium/index.tsx
+++ src/testcordplugins/AntilogPremium/index.tsx
@@ -68,7 +68,7 @@ async function antiLogDelete(channelId: string, messageId: string): Promise<bool
 }
 export default definePlugin({
     name: "AntilogPremium",
-    description: "Delete messages while hiding them from message loggers. Combines best anti-logging methods.",
+    description: "Delete messages while hiding them from message loggers. Combines best anti-logging methods. (its made to replace AntiLog, SilentDelete, and MLE's silent delete at once)",
     authors: [TestcordDevs.x2b],
     dependencies: ["MessagePopoverAPI", "CommandsAPI"],
     settings,
