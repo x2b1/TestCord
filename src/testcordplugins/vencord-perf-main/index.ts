9b60a5b96ea02fbeccf90c937e1270504b5e8331
x2b <mokihbbjjj@gmail.com>
2026-04-29 00:56:22 +0200

    try fix perf

--- src/testcordplugins/vencord-perf-main/index.ts
+++ src/testcordplugins/vencord-perf-main/index.ts
@@ -16,28 +16,7 @@ description: "[RISKY]Collection of small performance improvements[RISKY]",
             name: "void",
         },
     ],
-    patches: [{
-        // This module does a bunch of nasty spreads, so we kill parts of reactivity to save on performance
-        // We disallow calling get games and get usersPlaying to make sure they are not used ( i cant find any references anywhere )
-        // since their reactivity is broken
-        // Optionally we could still allow calling them, but if they get memoized its over
-        find: "=\"NowPlayingStore\"",
-        replacement: [{
-            match: /get games\(\)\{return \w+?\}/,
-            replace: "get games(){throw new Error('Vencord: perf: Not implemented')}",
-        }, {
-            match: /get usersPlaying\(\)\{return \w+?\}/,
-            replace: "get usersPlaying(){throw new Error('Vencord: perf: Not implemented')}",
-        }, {
-            match: /(\.gameId;return null!=\w\[\w\]&&\().+?,(.+?,)\w={\.\.\.\w\},/,
-            replace: (_, prev1, prev2) => prev1 + prev2,
-        }, {
-            match: /(startedPlaying:\w};return )\w=(\w),\w=(\w),(\w)=.+?(\w)=.+?!0/,
-            replace: (_, prev, gameId, activity, perGameId, perUser) => prev +
-                `${perGameId}[${gameId}] = { ...${perGameId}[${gameId}], [${activity}.userId]: ${activity} },` +
-                `${perUser}[${activity}.userId] = { gameId: ${gameId}, startedPlaying: ${activity}.startedPlaying }`,
-        }]
-    }, {
+patches: [{
         // The tooltip module uses flushSync to immediately push state changes which is kinda unnecessary
         // We bypass this by directly setting the state in a normal variable but still use setState to control the rendering
         find: "this.state.shouldShowTooltip!==",
