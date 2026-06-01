c75db7ba4dfc29fcefb1ff7016771d23c4c64a3b
DxRx99 <osher.zc@gmail.com>
2026-06-01 18:33:45 +0000

    fix stupid fucking button

--- src/testcordplugins/DirectMessageOpener/index.tsx
+++ src/testcordplugins/DirectMessageOpener/index.tsx
@@ -14,7 +14,20 @@ import { findComponentByCodeLazy } from "@webpack";
 import { Button, Forms, NavigationRouter, React, RestAPI, TextInput, Tooltip } from "@webpack/common";
 const FormText = Forms.FormText as any;
 
-const UserIcon = findComponentByCodeLazy("M14.99 11a1 1 0");
+const UserIcon = (props: any) => (
+    <svg
+        x="0"
+        y="0"
+        className={props.className}
+        aria-hidden="true"
+        width={props.width ?? 24}
+        height={props.height ?? 24}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+    >
+        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+    </svg>
+);
 const HeaderBarIcon = findComponentByCodeLazy(".HEADER_BAR_BADGE_TOP:", '.iconBadge,"top"');
 
 function getErrorMessage(err: any): string {
